538f59159ec600ebd1ee9c35ea1adc725bcfa173
ne0busin3ss <dave@dave-raymond.com>
2023-01-14 11:20:37 -0600

    Updated extensions

--- tkinter_example.py
+++ tkinter_example.py
@@ -18,7 +18,7 @@ class Application(Frame):
     def __init__(self, master=None):
         Frame.__init__(self, master)
         self.hi_there = Button(self)
-        self.QUIT = Button(self)
+        self.QUIT = Button(self)   
         self.pack()
         self.createWidgets()
 
