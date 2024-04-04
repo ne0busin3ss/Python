98c697fe74847184fcdfeebabd4dbd69799826c4
Dave Raymond <ne0busin3ss@gmail.com>
2024-04-04 17:13:17 -0500

    Add files via upload

---
+++ Python_Wrapper_1/Python_Wrappper_1.py
@@ -0,0 +1,26 @@
+import time
+
+def timeis(func):
+    '''Decorator that reports the execution time.'''
+
+    def wrap(*args, **kwargs):
+        start = time.time()
+        result = func(*args, **kwargs)
+        end = time.time()
+
+        print(func.__name__, end - start)
+        return result
+
+    return wrap
+
+
+@timeis
+def countdown(n):
+    '''Counts down'''
+    while n > 0:
+        n -= 1
+
+
+countdown(9)
+countdown(1000)
+ 
